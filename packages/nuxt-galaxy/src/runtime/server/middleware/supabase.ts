9a3864ac37da7f424e1d21f98489f120dd233b3a
Remi  PLANEL <rplanel@pasteur.fr>
2025-03-12 10:04:37 +0100

    fix(auth): relax cookie checking since github auth a suffix is added to cookies keys

--- packages/nuxt-galaxy/src/runtime/server/middleware/supabase.ts
+++ packages/nuxt-galaxy/src/runtime/server/middleware/supabase.ts
@@ -6,7 +6,13 @@ import { useSupabaseCookie } from '../../app/composables/useSupabaseCookie'
 export default defineEventHandler(async (event) => {
   const cookies = parseCookies(event)
   const { authCookieName } = useSupabaseCookie()
-  if (cookies[authCookieName]) {
+
+  const cookiesKeys = Object.keys(cookies)
+  const hasAuthCookie = cookiesKeys.find(key => key.startsWith(authCookieName))
+  // const hasCodeVerifier = cookiesKeys.find(key => key.endsWith('auth-token-code-verifier'))
+  if (hasAuthCookie
+  // && !hasCodeVerifier
+  ) {
     const user = await serverSupabaseUser(event)
     const client = await serverSupabaseClient<Database>(event)
     event.context.supabase = { user, client }
