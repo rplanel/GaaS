8c30f89bbab03b509a9be9dc02381af916c7c2a3
Remi  PLANEL <rplanel@pasteur.fr>
2025-04-24 22:46:11 +0200

    create a layer for wiki part

---
+++ packages/gaas-wiki/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+})
