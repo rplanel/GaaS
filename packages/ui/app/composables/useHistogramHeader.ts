8ac22e497bd5869e4bebf0210086683f8e276d64
Remi  PLANEL <rplanel@pasteur.fr>
2025-09-18 19:10:20 +0200

    fix

--- packages/ui/app/composables/useHistogramHeader.ts
+++ packages/ui/app/composables/useHistogramHeader.ts
@@ -1,7 +1,7 @@
 import type { Coordinator } from '@uwdata/mosaic-core'
 import type { GetHeaderParams, UseHeaderParams } from '../types/plotHeader'
 import { coordinator as defaultCoordinator, DuckDBWASMConnector } from '@uwdata/mosaic-core'
-import { h, onBeforeMount, ref } from 'vue'
+import { h, onBeforeMount, ref, watch } from 'vue'
 import PlotTableHeaderHistogram from '../components/plot/table/header/Histogram.vue'
 
 export function useHistogramHeader(params: UseHeaderParams) {
@@ -17,27 +17,34 @@ export function useHistogramHeader(params: UseHeaderParams) {
       mosaicCoordinator.value = c
     }
   })
-
-  function getHeader<T>(params: GetHeaderParams<T>): VNode {
-    const { column, label, variable } = params
-    if (!selection || !coordinator) {
-      console.warn('Missing required parameters for histogram header:', { table, selection, coordinator })
-      return h('div', { class: 'w-full' }, 'No data available')
+  function getHeaderFn() {
+    function _getHeader<T>(params: GetHeaderParams<T>): VNode {
+      const { column, label, variable } = params
+      if (!selection || !coordinator) {
+        console.warn('Missing required parameters for histogram header:', { table, selection, coordinator })
+        return h('div', { class: 'w-full' }, 'No data available')
+      }
+      return h('div', { class: 'w-full' }, [
+        h('div', { class: 'text-sm font-semibold' }, label),
+        // switch to filter data for getting nullish values
+        // h(USwitch, {
+        // }),
+        h(PlotTableHeaderHistogram, {
+          table,
+          selection,
+          variable,
+          coordinator,
+          width: column.getSize() - 32,
+        }),
+      ])
     }
-    return h('div', { class: 'w-full' }, [
-      h('div', { class: 'text-sm font-semibold' }, label),
-      // switch to filter data for getting nullish values
-      // h(USwitch, {
-      // }),
-      h(PlotTableHeaderHistogram, {
-        table,
-        selection,
-        variable,
-        coordinator,
-        width: column.getSize() - 32,
-      }),
-    ])
+    return _getHeader
   }
+
+  const getHeader = ref<ReturnType<typeof getHeaderFn>>(getHeaderFn())
+  watch(mosaicCoordinator, () => {
+    getHeader.value = getHeaderFn()
+  })
   return {
     getHeader,
   }
