4a6f9e608f40cece67ee98f0fe730b4f6670959b
Remi  PLANEL <rplanel@pasteur.fr>
2026-01-22 13:51:07 +0100

    validate ranges

--- packages/ui/app/components/Meili/index/filter/plot/ContinuousBuilder.vue
+++ packages/ui/app/components/Meili/index/filter/plot/ContinuousBuilder.vue
@@ -6,7 +6,6 @@ import type { FacetStats } from 'meilisearch'
 export interface ContinuousHeaderProps<T> {
   column: Column<T>
   label?: string
-  totalHits: number
   facetStats: FacetStats | undefined
   initialFacetStats?: FacetStats | undefined
 
@@ -15,103 +14,147 @@ export interface ContinuousHeaderProps<T> {
 const props = withDefaults(defineProps<ContinuousHeaderProps<T>>(), {
 })
 
-// Models
-const modelValue = defineModel<number[]>()
-// const modelFilter = defineModel<FacetFilter | undefined>('filter')
-
-// const facetStats = toRef(props, 'facetStats')
+const facetStats = toRef(props, 'facetStats')
 const initialFacetStats = toRef(props, 'initialFacetStats')
+
 const column = toRef(props, 'column')
 
-const initialMin = ref<number>(0)
-const initialMax = ref<number>(0)
+// ===================
+// facetStats related
+// ===================
 
-// const columnFacetStat = computed(() => {
-//   const facetStatsVal = toValue(facetStats)
-//   const columnId = column.value.id
-//   if (!facetStatsVal || !facetStatsVal[columnId]) {
-//     return undefined
-//   }
-//   return facetStatsVal[columnId]
-// })
-
-const initialColumnFacetStat = computed(() => {
-  const facetStatsVal = toValue(initialFacetStats)
-  const columnId = column.value.id
-  if (!facetStatsVal || !facetStatsVal[columnId]) {
+/**
+ * computed property of the facet name corresponding to the current column
+ */
+const facetName = computed(() => {
+  const columnVal = toValue(column)
+  if (!columnVal || !columnVal.id) {
     return undefined
   }
-  return facetStatsVal[columnId]
+  return columnVal.id
 })
 
-// const minMaxValues = computed(() => {
-//   const columnFacetStatVal = toValue(columnFacetStat)
+const columnFacetStat = computed(() => {
+  const facetStatsVal = toValue(facetStats)
+  const facetNameVal = toValue(facetName)
+  if (!facetStatsVal || !facetNameVal || !facetStatsVal[facetNameVal]) {
+    return undefined
+  }
+  return facetStatsVal[facetNameVal]
+})
 
-//   if (!columnFacetStatVal) {
-//     return { min: 0, max: 0 }
-//   }
-//   const { min, max } = columnFacetStatVal
-//   return { min: min as number, max: max as number }
-// })
+/**
+ * initial facet stats
+ */
 
-const initialMinMaxValues = computed(() => {
-  const columnFacetStatVal = toValue(initialColumnFacetStat)
+// const initialFacetStats = ref<FacetStats | undefined>(undefined)
 
+onBeforeMount(() => {
+  initialFacetStats.value = undefined
+})
+
+/**
+ * Type guard to validate that a range is defined and contains valid numbers
+ */
+function isValidRange(range: number[] | undefined) {
+  return !!range && range.length === 2
+    && typeof range[0] === 'number' && typeof range[1] === 'number'
+    && !Number.isNaN(range[0]) && !Number.isNaN(range[1])
+}
+
+/**
+ * computed property of the initial column facet stats. It corresponds to the facet stat
+ * if the current column.
+ */
+const initialColumnFacetStat = computed(() => {
+  const initialFacetStatsVal = toValue(initialFacetStats)
+  const facetNameVal = toValue(facetName)
+  if (!initialFacetStatsVal || !facetNameVal || !initialFacetStatsVal[facetNameVal]) {
+    return undefined
+  }
+  return initialFacetStatsVal[facetNameVal]
+})
+
+const initialRangeFacet = computed<number[] | undefined>(() => {
+  const initialColumnFacetStatVal = toValue(initialColumnFacetStat)
+  if (!initialColumnFacetStatVal) {
+    return undefined
+  }
+  const { min, max } = initialColumnFacetStatVal
+  return [min as number, max as number]
+})
+
+const initialValidatedRangeFromFacet = computed(() => {
+  const initialRangeFacetVal = toValue(initialRangeFacet)
+  if (isValidRange(initialRangeFacetVal)) {
+    return initialRangeFacetVal
+  }
+  return undefined
+})
+
+const rangeFromFacet = computed<number[] | undefined>(() => {
+  const columnFacetStatVal = toValue(columnFacetStat)
   if (!columnFacetStatVal) {
-    return { min: 0, max: 0 }
+    return undefined
   }
   const { min, max } = columnFacetStatVal
-  return { min: min as number, max: max as number }
+  return [min as number, max as number]
 })
+
+const validatedRangeFromFacet = computed(() => {
+  const rangeFromFacetVal = toValue(rangeFromFacet)
+  if (isValidRange(rangeFromFacetVal)) {
+    return rangeFromFacetVal
+  }
+  return undefined
+})
+
+/**
+ * Range model
+ */
+const modelValue = defineModel<number[] | undefined>(undefined)
+
+// Only initialize modelValue when it's truly not set (undefined or empty)
+watch(rangeFromFacet, (rangeFromFacetVal) => {
+  if (!modelValue.value && isValidRange(rangeFromFacetVal)) {
+    modelValue.value = rangeFromFacetVal
+  }
+}, { immediate: true })
 
 const rangeMin = computed({
   get: () => modelValue.value?.[0],
   set: (value: number) => {
-    if (modelValue.value?.[1]) {
+    if (modelValue.value?.[1] !== undefined) {
       modelValue.value = [value, modelValue.value[1]]
     }
-    modelValue.value = []
   },
 })
 
 const rangeMax = computed({
   get: () => modelValue.value?.[1],
   set: (value: number) => {
-    if (modelValue.value?.[0]) {
+    if (modelValue.value?.[0] !== undefined) {
       modelValue.value = [modelValue.value[0], value]
     }
   },
 })
 
-onMounted(() => {
-  const { min, max } = initialMinMaxValues.value
-  initialMin.value = min
-  initialMax.value = max
-  modelValue.value = [min, max]
-})
-
-watch(initialMinMaxValues, (newVal) => {
-  const { min, max } = newVal
-  initialMin.value = min
-  initialMax.value = max
-  modelValue.value = [min, max]
-  // rangeMin.value = min
-  // rangeMax.value = max
-})
-
 // Watch column visibility and reset slider when hidden
-watch(() => column.value.getIsVisible(), (isVisible) => {
+watch(() => column.value.getIsVisible(), (isVisible, oldIsVisible) => {
   if (!isVisible) {
-    modelValue.value = []
+    modelValue.value = undefined
+  }
+  if (isVisible && !oldIsVisible) {
+    const rangeFromFacetVal = toValue(rangeFromFacet)
+    if (isValidRange(rangeFromFacetVal)) {
+      modelValue.value = rangeFromFacetVal
+    }
   }
 })
 
-// onBeforeUnmount(() => {
-//   console.log('ContinuousHeader before unmount')
-//   modelValue.value = []
-// })
 onUnmounted(() => {
-  modelValue.value = []
+  modelValue.value = undefined
+  initialFacetStats.value = undefined
 })
 
 const inputNumberProps = ref<InputNumberProps>({
@@ -120,19 +163,43 @@ const inputNumberProps = ref<InputNumberProps>({
   variant: 'soft',
 
 })
+
+const step = computed(() => {
+  const validatedRange = toValue(validatedRangeFromFacet)
+  if (!validatedRange) {
+    return 1
+  }
+  const [min, max] = validatedRange
+  return typeof min === 'number' && typeof max === 'number' ? (max - min) / 100 : 1
+})
 </script>
 
 <template>
   <div>
-    <div class="flex flex-col gap-3 w-full">
+    <div v-if="validatedRangeFromFacet && initialValidatedRangeFromFacet" class="flex flex-col gap-3 w-full">
       <div class="flex flex-row gap-1 justify-between">
-        <UInputNumber v-model="rangeMin" :min="initialMin" :max="initialMax" v-bind="inputNumberProps" />
-        <UInputNumber v-model="rangeMax" :min="initialMin" :max="initialMax" v-bind="inputNumberProps" />
+        <UInputNumber
+          v-model="rangeMin"
+          name="rangeMin"
+          :min="initialValidatedRangeFromFacet[0]"
+          :max="initialValidatedRangeFromFacet[1]"
+          v-bind="inputNumberProps"
+        />
+        <UInputNumber
+          v-model="rangeMax"
+          name="rangeMax"
+          :min="initialValidatedRangeFromFacet[0]"
+          :max="initialValidatedRangeFromFacet[1]"
+          v-bind="inputNumberProps"
+        />
       </div>
       <div>
         <USlider
-          v-model="modelValue" :min="initialMin" :max="initialMax"
-          :step="((initialMax || 0) - (initialMin || 0)) / 100"
+          v-if="modelValue"
+          v-model="modelValue"
+          :min="initialValidatedRangeFromFacet[0]"
+          :max="initialValidatedRangeFromFacet[1]"
+          :step="step"
         />
       </div>
     </div>
