74ac1d6b9347d5a279c8f92eb6c7ea8649672b29
Remi  PLANEL <rplanel@pasteur.fr>
2026-02-09 16:08:46 +0100

    bucket id = bucket name

--- packages/nuxt-galaxy/src/runtime/server/db/migrations/20260206115542_buckets/migration.sql
+++ packages/nuxt-galaxy/src/runtime/server/db/migrations/20260206115542_buckets/migration.sql
@@ -1,10 +1,10 @@
 -- Custom SQL migration file, put your code below! --
 
 insert into storage.buckets (id, name)
-values ('analysis_files', 'Analysis Files')
+values ('analysis_files', 'analysis_files')
 on conflict do nothing;
 
 
 insert into storage.buckets (id, name, public)
-values ('public_data', 'Public Data', true)
+values ('public_data', 'public_data', true)
 on conflict do nothing;
