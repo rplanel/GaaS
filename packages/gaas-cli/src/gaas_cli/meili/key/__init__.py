3a01598ea2622b3702b1ccf89e8f0c6152a7eb6e
Remi  PLANEL <rplanel@pasteur.fr>
2026-02-10 14:38:57 +0100

    can add nuxt prefix to meili env vars

--- packages/gaas-cli/src/gaas_cli/meili/key/__init__.py
+++ packages/gaas-cli/src/gaas_cli/meili/key/__init__.py
@@ -10,6 +10,9 @@ app = typer.Typer(no_args_is_help=True)
 @app.command()
 def env_var(
     ctx: typer.Context,
+    nuxt_prefix: Annotated[
+        bool, typer.Option("--nuxt-prefix", help="Add NUXT_ prefix to env vars")
+    ] = False,
     output: Annotated[
         Path,
         typer.Option(
@@ -21,9 +24,9 @@ def env_var(
 ):
     client = ctx.obj["client"]
     keys = client.get_keys()
-
+    nuxt_prefix_str = "NUXT_" if nuxt_prefix else ""
     api_key = [res.key for res in keys.results if res.name == "Default Search API Key"]
     if len(api_key) == 1:
         with open(output, "a") as outfile:
-            outfile.write(f"MEILI_HOST={ctx.obj['host']}\n")
-            outfile.write(f"MEILI_API_KEY={api_key[0]}\n")
+            outfile.write(f"{nuxt_prefix_str}MEILI_HOST={ctx.obj['host']}\n")
+            outfile.write(f"{nuxt_prefix_str}MEILI_API_KEY={api_key[0]}\n")
